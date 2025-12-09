2c60379e6ef3a80ec7091954a03abf4b3b4332f7
Steve Reece <steve@scrumfish.com>
2025-12-09 04:02:39 -0700

    Lab 5 - Initial Commit

--- lab-5/dog-api/src/main/java/com/example/dog_api/repository/CanineContract.java
+++
@@ -1,7 +0,0 @@
-package com.example.dog_api.repository;
-
-public interface CanineContract<T> {
-    public void save(T canine);
-    public T findById(Long id);
-    public java.util.Collection<T> getAll();
-}
