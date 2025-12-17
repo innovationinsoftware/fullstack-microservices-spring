ce4c6eb47f4c00424052bcd437d2cd6c4704e4b1
Steve Reece <steve@scrumfish.com>
2025-12-17 01:44:07 -0700

    Finished instructions.

--- lab-7/README.md
+++ lab-7/README.md
@@ -17,5 +17,24 @@ In this lab you will add OpenAPI support to your microservice, then test your se
 2. Add a PUT method that will update an existing dog based on id.
 3. Use the Swagger page to test and verify the new API methods.
 
+## Step 3 - Add additional information
+It is possible to tag the services, both at the class level, and at the method level. For microservices, tags at the class level tend to be less useful, as their may only be one class published. We'll add some tags at the method level.
+
+1. Tag all the methods that read data with a 'Read Operations' tag.
+```java
+@Tag(name = "Read Operations")
+```
+2. Tag all the methods that modify data with a 'Write Operations' tag.
+3. Build and run and view the difference on the Swagger page.
+
+## Step 4 - Document the Dog model
+1. Update the Dog model to have a minium and maximum string length for 'breed'.
+```java
+@Size(min = 0, max = 50)
+```
+2. Build and run.
+3. Scroll down to the schema portion at the bottom of the Swagger page and expand the Dog model.
+4. Notice the additional information added to the model definition.
+
 # Conclusion
 Congratulations, you've added OpenAPI support to your microservice and used Swagger to help you add two new methods.
--- lab-7/dog-api/src/main/java/com/example/dog_api/controller/DogController.java
+++ lab-7/dog-api/src/main/java/com/example/dog_api/controller/DogController.java
@@ -3,6 +3,9 @@ package com.example.dog_api.controller;
 import com.example.dog_api.entity.Dog;
 import com.example.dog_api.repository.CanineContract;
 import com.example.dog_api.repository.DogRepository;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -17,11 +20,14 @@ public class DogController {
         this.dogRepository = dogRepository;
     }
 
+
+    @Tag(name = "Read Operations")
     @GetMapping("/{id}")
     public Dog getDogById(@PathVariable Long id) {
         return dogRepository.findById(id);
     }
 
+    @Tag(name = "Read Operations")
     @GetMapping
     public Collection<Dog> getAllDogs() {
         if (dogRepository instanceof DogRepository) {
@@ -30,16 +36,19 @@ public class DogController {
         return null;
     }
 
+    @Tag(name = "Write Operations")
     @PostMapping
     public void addDog(@RequestBody Dog dog) {
         dogRepository.save(dog);
     }
 
+    @Tag(name = "Write Operations")
     @PutMapping("/{id}")
     public void updateDog(@PathVariable Long id, @RequestBody Dog dog) {
         dogRepository.update(id, dog);
     }
 
+    @Tag(name = "Write Operations")
     @DeleteMapping("/{id}")
     public void deleteDog(@PathVariable Long id) {
         dogRepository.deleteById(id);
